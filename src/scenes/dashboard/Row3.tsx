b7176040016ac76eafe11d38a2d50eb1453600b9
mikel-js <michaelcastro.js@gmail.com>
2024-01-10 08:38:20 +0200

    add imports and variables

--- src/scenes/dashboard/Row3.tsx
+++ src/scenes/dashboard/Row3.tsx
@@ -1,8 +1,89 @@
+import BoxHeader from '@/components/BoxHeader';
 import DashboardBox from '@/components/DashboardBox';
+import FlexBetween from '@/components/FlexBetween';
+import {
+  useGetKpisQuery,
+  useGetProductsQuery,
+  useGetTransactionsQuery,
+} from '@/state/api';
+import { Box, Typography, useTheme } from '@mui/material';
+import { DataGrid, GridCellParams } from '@mui/x-data-grid';
+import React, { useMemo } from 'react';
+import { Cell, Pie, PieChart } from 'recharts';
 
-type Props = {};
+const Row3 = () => {
+  const { palette } = useTheme();
+  const pieColors = [palette.primary[800], palette.primary[500]];
 
-const Row3 = (props: Props) => {
+  const { data: kpiData } = useGetKpisQuery();
+  const { data: productData } = useGetProductsQuery();
+  const { data: transactionData } = useGetTransactionsQuery();
+
+  const pieChartData = useMemo(() => {
+    if (kpiData) {
+      const totalExpenses = kpiData[0].totalExpenses;
+      return Object.entries(kpiData[0].expensesByCategory).map(
+        ([key, value]) => {
+          return [
+            {
+              name: key,
+              value: value,
+            },
+            {
+              name: `${key} of Total`,
+              value: totalExpenses - value,
+            },
+          ];
+        }
+      );
+    }
+  }, [kpiData]);
+
+  const productColumns = [
+    {
+      field: '_id',
+      headerName: 'id',
+      flex: 1,
+    },
+    {
+      field: 'expense',
+      headerName: 'Expense',
+      flex: 0.5,
+      renderCell: (params: GridCellParams) => `$${params.value}`,
+    },
+    {
+      field: 'price',
+      headerName: 'Price',
+      flex: 0.5,
+      renderCell: (params: GridCellParams) => `$${params.value}`,
+    },
+  ];
+
+  const transactionColumns = [
+    {
+      field: '_id',
+      headerName: 'id',
+      flex: 1,
+    },
+    {
+      field: 'buyer',
+      headerName: 'Buyer',
+      flex: 0.67,
+    },
+    {
+      field: 'amount',
+      headerName: 'Amount',
+      flex: 0.35,
+      renderCell: (params: GridCellParams) => `$${params.value}`,
+    },
+    {
+      field: 'productIds',
+      headerName: 'Count',
+      flex: 0.1,
+      renderCell: (params: GridCellParams) =>
+        (params.value as Array<string>).length,
+    },
+  ];
   return (
     <>
       <DashboardBox gridArea='g'></DashboardBox>
